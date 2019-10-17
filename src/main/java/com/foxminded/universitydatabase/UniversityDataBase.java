a7b30719276314e6446dcc2df1ee764762c0f20a
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2019-10-17 17:39:50 +0300

    Connecting logic was refactored. New class was created for connection. New package was created for all manipulations with data base

--- src/main/java/com/foxminded/universitydatabase/UniversityDataBase.java
+++ src/main/java/com/foxminded/universitydatabase/UniversityDataBase.java
@@ -1,19 +1,7 @@
 package com.foxminded.universitydatabase;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class UniversityDataBase {
-    private static final String URL = "jdbc:postgresql://localhost:5432/university_db";
-    private static final String USER_NAME = "postgres";
-    private static final String PASSWORD = "root";
-
     public static void main(String[] args) {
-        try {
-            Connection postgreSQLConnection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("Sorry (; Connection troubles");
-        }
+
     }
 }
---
+++ src/main/java/com/foxminded/universitydatabase/db_layer/connections/PostgreSQLConnection.java
@@ -0,0 +1,15 @@
+package com.foxminded.universitydatabase.db_layer.connections;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/university_db";
+    private static final String USER_NAME = "postgres";
+    private static final String PASSWORD = "root";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+    }
+}
