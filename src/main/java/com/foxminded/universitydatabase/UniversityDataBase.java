e6f181d73cb03f6f7d33274cd488ea2cdf4647ee
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2019-10-17 16:58:50 +0300

    PostgreSQL was connected

--- src/main/java/com/foxminded/universitydatabase/UniversityDataBase.java
+++ src/main/java/com/foxminded/universitydatabase/UniversityDataBase.java
@@ -1,7 +1,19 @@
 package com.foxminded.universitydatabase;
 
-public class UniversityDataBase {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class UniversityDataBase {
+    private static final String URL = "jdbc:postgresql://localhost:5432/university_db";
+    private static final String USER_NAME = "postgres";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        try {
+            Connection postgreSQLConnection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Sorry (; Connection troubles");
+        }
     }
 }
