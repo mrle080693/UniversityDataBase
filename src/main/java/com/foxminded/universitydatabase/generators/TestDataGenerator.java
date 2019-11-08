8ebaf3de26b1b434d5b745c3f6e5081ecc4de85c
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2019-11-08 16:11:05 +0200

    TestDataGenerator class logic was refactored

--- src/main/java/com/foxminded/universitydatabase/generators/TestDataGenerator.java
+++ src/main/java/com/foxminded/universitydatabase/generators/TestDataGenerator.java
@@ -4,10 +4,49 @@ import com.foxminded.universitydatabase.db_layer.managers.UniversityDBManager;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 
 public class TestDataGenerator {
-    private RandomsGenerator randomsGenerator = new RandomsGenerator();
+    private Random random = new Random();
+    private static String[] names = new String[10];
+    private static String[] surnames = new String[10];
+    private static String[] facultyNames = new String[10];
     private UniversityDBManager universityDBManager = new UniversityDBManager();
+
+    static {
+        names[0] = "StudentsName1";
+        names[1] = "StudentsName2";
+        names[2] = "StudentsName3";
+        names[3] = "StudentsName4";
+        names[4] = "StudentsName5";
+        names[5] = "StudentsName6";
+        names[6] = "StudentsName7";
+        names[7] = "StudentsName8";
+        names[8] = "StudentsName9";
+        names[9] = "StudentsName10";
+
+        surnames[0] = "StudentsSurname1";
+        surnames[1] = "StudentsSurname2";
+        surnames[2] = "StudentsSurname3";
+        surnames[3] = "StudentsSurname4";
+        surnames[4] = "StudentsSurname5";
+        surnames[5] = "StudentsSurname6";
+        surnames[6] = "StudentsSurname7";
+        surnames[7] = "StudentsSurname8";
+        surnames[8] = "StudentsSurname9";
+        surnames[9] = "StudentsSurname10";
+
+        facultyNames[0] = "Faculty1";
+        facultyNames[1] = "Faculty2";
+        facultyNames[2] = "Faculty3";
+        facultyNames[3] = "Faculty4";
+        facultyNames[4] = "Faculty5";
+        facultyNames[5] = "Faculty6";
+        facultyNames[6] = "Faculty7";
+        facultyNames[7] = "Faculty8";
+        facultyNames[8] = "Faculty9";
+        facultyNames[9] = "Faculty10";
+    }
 
     public TestDataGenerator() throws SQLException {
     }
@@ -21,33 +60,21 @@ public class TestDataGenerator {
 
     private void generateGroups() throws SQLException {
         for (int i = 0; i < 10; i++) {
-            String groupName = randomsGenerator.generateRandomGroupName();
+            String groupName = generateRandomGroupName();
             universityDBManager.createGroup(groupName);
         }
     }
 
     private void generateFaculties() throws SQLException {
-        String[] faculyNames = new String[10];
-        faculyNames[0] = "Faculty1";
-        faculyNames[1] = "Faculty2";
-        faculyNames[2] = "Faculty3";
-        faculyNames[3] = "Faculty4";
-        faculyNames[4] = "Faculty5";
-        faculyNames[5] = "Faculty6";
-        faculyNames[6] = "Faculty7";
-        faculyNames[7] = "Faculty8";
-        faculyNames[8] = "Faculty9";
-        faculyNames[9] = "Faculty10";
-
         for (int i = 0; i < 10; i++) {
-            universityDBManager.createFaculty(faculyNames[i], "Without description");
+            universityDBManager.createFaculty(facultyNames[i], "Without description");
         }
     }
 
     private void generateStudents() throws SQLException {
         for (int i = 0; i < 200; i++) {
-            String name = randomsGenerator.generateRandomName();
-            String surname = randomsGenerator.generateRandomSurname();
+            String name = generateRandomName();
+            String surname = generateRandomSurname();
 
             universityDBManager.createStudent(name, surname);
         }
@@ -66,5 +93,29 @@ public class TestDataGenerator {
         }
 
         universityDBManager.disbandNotMoreXGroups(10);
+    }
+
+    private String generateRandomGroupName() {
+        return String.valueOf(getRandomCharacterFromInput("ABCDEFGHIJ")) +
+                getRandomCharacterFromInput("ABCDEFGHIJ") + "-" +
+                getRandomCharacterFromInput("1234567890") +
+                getRandomCharacterFromInput("1234567890");
+    }
+
+    private String generateRandomName() {
+        return getRandomArrayElementFromInput(names);
+    }
+
+    private String generateRandomSurname() {
+        return getRandomArrayElementFromInput(surnames);
+    }
+
+
+    private Character getRandomCharacterFromInput(String input) {
+        return input.charAt(random.nextInt(input.length()));
+    }
+
+    private String getRandomArrayElementFromInput(String[] input) {
+        return input[random.nextInt(input.length - 1)];
     }
 }
