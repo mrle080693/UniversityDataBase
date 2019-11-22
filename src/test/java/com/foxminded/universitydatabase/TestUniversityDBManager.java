920260cabbab004eb57e38812d53868b530bb0d2
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2019-11-22 14:51:03 +0200

    DBUnit troubles

--- src/test/java/com/foxminded/universitydatabase/TestUniversityDBManager.java
+++ src/test/java/com/foxminded/universitydatabase/TestUniversityDBManager.java
@@ -1,58 +1,67 @@
 package com.foxminded.universitydatabase;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 import com.foxminded.universitydatabase.db_layer.managers.UniversityDBManager;
-import config.DBUnitConfig;
-import org.dbunit.Assertion;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.List;
+import javax.sql.DataSource;
+import java.io.File;
+import java.sql.ResultSet;
 
-public class TestUniversityDBManager extends DBUnitConfig {
-
+public class TestUniversityDBManager {
+    private static final String JDBC_DRIVER = org.postgresql.Driver.class.getName();
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/university_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
     private UniversityDBManager universityDBManager = new UniversityDBManager();
 
-    // переопределяем метод для того, чтобы передать в него наш набор данных для конкретного теста.
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-
-        // создает набор данных с переданного ему в потоке файла xml который содержит в себе описание данных,
-        // которые должны будут записаны в соответствующие таблички.
-        // Состоит с тега верхнего уровня dataset внутри которого находятся теги с атрибутами.
-        // Название тега –  имя таблички, атрибуты внутри – атрибуты таблички в БД.
-        beforeData = new FlatXmlDataSetBuilder().build(
-                Thread.currentThread().getContextClassLoader()
-                        .getResourceAsStream("C:\\Java\\Projects\\Foxminded\\Mentoring\\UniversityDataBase\\src\\test\\resources\\person-data.xml"));
-
-        tester.setDataSet(beforeData);
-        tester.onSetup();
+    @BeforeClass
+    public static void createSchema() throws Exception {
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, JDBC_DRIVER);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, JDBC_URL);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, USER);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, PASSWORD);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "chema.sql");
     }
 
-    public TestUniversityDBManager(String name) {
-        super(name);
+    @Before
+    public void importDataSet() throws Exception {
+        IDataSet dataSet = readDataSet();
+        cleanlyInsert(dataSet);
+    }
+
+    private IDataSet readDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(new File("dataset.xml"));
+    }
+
+    private void cleanlyInsert(IDataSet dataSet) throws Exception {
+        IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
     }
 
     @Test
-    public void testGetAll() throws Exception {
-        // создаем набор ожидаемых данных
-        IDataSet expectedData = new FlatXmlDataSetBuilder().build(
+    public void findsAndReadsExistingPersonByFirstName() throws Exception {
+        ResultSet actual = universityDBManager.getAllStudents();
+
+        FlatXmlDataSet expected = new FlatXmlDataSetBuilder().build(
                 Thread.currentThread().getContextClassLoader()
-                        .getResourceAsStream("com/devcolibri/entity/person/person-data.xml"));
+                        .getResourceAsStream("C:\\Java\\Projects\\Foxminded\\Mentoring\\UniversityDataBase\\src\\test\\resources\\dataset.xml"));
 
-        // Создаем набор данных с существующих данных в БД:
-        IDataSet actualData = tester.getConnection().createDataSet();
-
-        // В случаях, когда сравниваются таблички или наборы данных в которых присутствует поле
-        // типа auto increment могут быть ошибки при прямом сравнивании так как,
-        // при удалении одного поля и добавления нового с БД,  индекс будет расти,
-        // что не будет соответствовать  индексу с набора данных в xml файле.
-        // Поэтому, одним решением есть игнорирование поля, как мы делали выше.
-        String[] ignore = {"id"};
-        Assertion.assertEqualsIgnoreCols(expectedData, actualData, "students", ignore);
+        assertEquals(expected, actual);
     }
-
-    //others tests
 }
